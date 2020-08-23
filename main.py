90600604045c83b0e04c86eb0ef6ce2c2ff94b57
Pranav Srivastava <pranav.srivastava@kpn.com>
2020-08-23 22:50:03 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from resources import index_html_response, enter_details, hello_html_response, login_details, chatbot_ques_html, chatbot_ans_html
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
+
+@app.get("/hello/", response_class=HTMLResponse)
+async def hello():
+    return hello_html_response()
+
+@app.get("/index/", response_class=HTMLResponse)
+async def read_items():
+    return index_html_response()
+
+@app.get("/login/", response_class=HTMLResponse)
+async def details():
+    return enter_details()
+
+@app.get("/chatbot_question/", response_class=HTMLResponse)
+async def chatbot_ques():
+    return chatbot_ques_html()
+
+@app.post("/chatbot_answer/", response_class=HTMLResponse)
+async def chatbot_ans(request: Request, question: str = Form(...)):
+    return chatbot_ans_html(question)
+
+@app.post("/check_login/",response_class=HTMLResponse)
+async def check_details(request: Request, username: str = Form(...), password: str = Form(...)):
+    print(username)
+    print(password)
+    #return login_details(request)
+
+@app.post("/form")
+def form_post(request: Request, username: str = Form(...)):
+    result = username
+    print(username)
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
---
+++ resources.py
@@ -0,0 +1,92 @@
+from fastapi.responses import HTMLResponse
+import urllib
+from bs4 import BeautifulSoup
+from fastapi import Request as request
+from fastapi import FastAPI, Form
+from starlette.requests import Request
+from collections import defaultdict
+
+def index_html_response():
+    html_content = """
+        <html>
+            <body>
+                <button onclick="window.location.href='/index'">Home</button>
+                <button onclick="window.location.href='/hello'">Hello</button><br><br><br>
+                <button onclick="window.location.href='/chatbot_question'">Chatbot</button><br><br><br>
+                <button onclick="window.location.href='/login'">Details</button><br><br><br>
+
+            </body>
+        </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+def hello_html_response():
+    html_content = """
+        <html>
+            <body>
+                <button onclick="window.location.href='/index'">Home</button><br>
+                <b>hello world!</b>
+            </body>
+        </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+def enter_details():
+    html_content="""<form action=/check_login method='POST'> 
+                    <label> Username </label> 
+                    <input type='text' name='username'> 
+                    <label> Password </label> 
+                    <input type='text' name='password'> 
+                
+                    <button type='submit'> Submit </button> 
+                    </form> """
+ 
+    soup = BeautifulSoup(html_content)
+    print(soup.get_text())
+    #print("html content",html_content)
+    return HTMLResponse(content=html_content, status_code=200)
+
+def chatbot_ques_html():
+    html_content="""<form action=/chatbot_answer method='POST'> 
+                    <label> Ask anything </label> 
+                    <input type='text' name='question'> 
+                
+                    <button type='submit'> Submit </button> 
+                    </form> """
+
+    return HTMLResponse(content=html_content, status_code=200)
+
+def chatbot_ans_html(question):
+    with open("chatbot_sample_intents.csv", "r") as f:
+        content = f.readlines()
+
+    text_fields = question.split(" ")
+    predict_answer = {}
+    for line in content:
+        fields = line.strip().split(",")
+        target = fields[0]
+        count = sum([1 for word in text_fields if word in fields[1:]])
+        predict_answer[target] = count
+
+    answer = max(predict_answer, key=predict_answer.get)
+
+    html_content="""<html>
+            <body>
+            """ + answer + """
+            <br><br><button onclick="window.location.href='/chatbot_question'">Another question</button><br><br>
+            <button onclick="window.location.href='/index'">Home</button><br>
+
+            </body>
+        </html> """
+
+    return HTMLResponse(content=html_content, status_code=200)
+
+def login_details(request):
+
+    print("request is--->",Request.stream('username'))
+    #username=request.form['username']
+    #password = request.form['password']
+    #print("username is--->",username)
+    #print(password)
+    #return username, password
