6982da9274680564b1af4bbd04a531c7207a810a
Pranav Srivastava <pranav.srivastava@kpn.com>
2020-08-23 23:02:59 +0200

    updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI,Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
